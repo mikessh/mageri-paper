2c60f3f35a171b5b5acd2a58810d201cf3b18121
mikessh <mikhail.shugay@gmail.com>
2016-12-16 01:35:59 +0300

    More cleanup, done with figures for tonight

--- suppl/processing/download.sh
+++
@@ -1,4 +0,0 @@
-for p in PRJNA272736
-do
-   ../edirect/esearch -db sra -query $p | ../edirect/efetch --format runinfo | cut -d ',' -f 1 | grep SRR | xargs ../sra-tk/bin/fastq-dump --split-files
-done
